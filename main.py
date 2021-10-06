ad3acc21e1846eeea6c087e602e4ea8184fc1338
AAverianova <91321737+AAverianova@users.noreply.github.com>
2021-10-06 19:28:28 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,20 +3,36 @@ import math
 
 class Dots(object):
 
+
     def __init__(self, x, y):
 
         self.x = x
         self.y = y
-
+# Если получать x и y из вне при инициализации, то тогда придется все внешние программы переделывать при измении класса. что плохо.
+# Можно сделать так:
+# def __init__(self):
+#
+#        self.x = float(input('Введите координату x: '))
+#        self.y= float(input('Введите координату y: '))
+        
     def show(self):
 
         return '({}; {})'.format(self.x, self.y)
 
-
+    def distance (self, A):
+        return math.sqrt(abs(self.x - A.x)**2 + abs(self.y-A.y)**2)
+    
+    
 class Triangle(object):
 
     def __init__(self, ax, ay, bx, by, cx, cy):
-
+# Тоже самое. При инициализации я должна знать какие данные что означают. А если я не могу посмотреть? Коммерческая тайна?
+#     def __init__(self):
+#
+#        self.A = Dots()
+#        self.B = Dots()
+#        self.C = Dots()
+        
         self.A = (ax, ay)
         self.B = (bx, by)
         self.C = (cx, cy)
@@ -26,6 +42,7 @@ class Triangle(object):
         return s
 
     def per(self):
+ #  расчет расстояния следует вынести в один метод. Т.к. оно повторяется, и если изменится, то менять придется только в одном месте.
         ab = math.sqrt(abs(self.A[0] - self.B[0])**2 + abs(self.A[1] - self.B[1])**2)
         bc = math.sqrt(abs(self.B[0] - self.C[0])**2 + abs(self.B[1] - self.C[1])**2)
         ac = math.sqrt(abs(self.A[0] - self.C[0])**2 + abs(self.A[1] - self.C[1])**2)
@@ -40,32 +57,36 @@ class Triangle(object):
 class Rect(object):
 
     def __init__(self, ax, ay, bx, by, cx, cy, dx, dy):
-        self.A = (ax, ay)
-        self.B = (bx, by)
-        self.C = (cx, cy)
-        self.D = (dx, dy)
+        print('Точка 1')
+        self.A = Dots()
+        print('Точка 2')
+        self.B = Dots()
+        print('Точка 3')
+        self.C = Dots()
+        print('Точка 4')
+        self.D = Dots()
+
 
     def area(self):
-        ax = self.A[0]
-        ay = self.A[1]
-        bx = self.B[0]
-        by = self.B[1]
-        cx = self.C[0]
-        cy = self.C[1]
-        dx = self.D[0]
-        dy = self.D[1]
-
-        s = abs(ax * by + bx * cy + cx * dy + dx * ay - bx * ay - cx * by - dx * cy - ax * dy)/2
+        ab =  self.A.distance(self.B)
+        cd =  self.C.distance(self.D)
+        s = ab*cd
         return s
+
 
 
 while True:
     ans = input('С чем вы хотите работать?\n-точка\n-треугольник\nпрямоугольник\n--')
 
     if ans == 'точка':
+        # Если это класс точка, то необходимо использовать методы класса точка для работы с полями этого класса. Когда будут трехмерные координаты, всю программу придется переписывать.
+        # А если будет требование что читать не из консоли а из файла, то снова все эти условия менять и переписывать?
+        # Если будет запрос данных в инициализации, то все упрощается
+        #  dot = Dots()
         dot_x = float(input('Введите координату x: '))
         dot_y = float(input('Введите координату y: '))
         dot = Dots(dot_x, dot_y)
+        
         print('Координаты точки:', dot.show())
     elif ans == 'треугольник':
         dot_a_x = float(input('Введите координату x вершины А: '))
@@ -86,19 +107,7 @@ while True:
             print('Вы ввели прямую')
             continue
     elif ans == 'прямоугольник':
-        dax = float(input('Введите координату x вершины А: '))
-        day = float(input('Введите координату y вершины А: '))
-        dbx = float(input('Введите координату x вершины B: '))
-        dby = float(input('Введите координату y вершины B: '))
-        dcx = float(input('Введите координату x вершины C: '))
-        dcy = float(input('Введите координату y вершины C: '))
-        ddx = float(input('Введите координату x вершины D: '))
-        ddy = float(input('Введите координату y вершины D: '))
-        A = Dots(dax, day)
-        B = Dots(dbx, dby)
-        C = Dots(dcx, dcy)
-        D = Dots(ddx, ddy)
-        ABCD = Rect(A.x, A.y, B.x, B.y, C.x, C.y, D.x, D.y)
+        ABCD = Rect()
         print(ABCD.area())
 
     q = input('Если хотите выйти введите q: ')
